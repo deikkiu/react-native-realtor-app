b77919e02829a0d9ab1593f9a4e659c570d07c6e
Timur Smagulov <deikku@mail.ru>
2023-02-20 01:29:15 +0600

    Fix: fixed screens navigation

--- navigation/Navigation.jsx
+++ navigation/Navigation.jsx
@@ -1,5 +1,33 @@
-const Navigation = () => {
-  return <div>Navigation</div>;
-};
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Tabs } from './Tabs';
+import { Login, SignUp, ForgotPassword, Announcement } from '../screens';
 
-export default Navigation;
+const Stack = createNativeStackNavigator();
+
+export const Navigation = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Tabs"
+        component={Tabs}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen name="Announcement" component={Announcement} />
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{ title: 'Войти' }}
+      />
+      <Stack.Screen
+        name="SignUp"
+        component={SignUp}
+        options={{ title: 'Зарегистрироваться' }}
+      />
+      <Stack.Screen
+        name="ForgotPassword"
+        component={ForgotPassword}
+        options={{ title: 'Восстановить пароль' }}
+      />
+    </Stack.Navigator>
+  );
+};
--- navigation/Tabs.jsx
+++ navigation/Tabs.jsx
@@ -1,4 +1,3 @@
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {
   CreateScreen,
@@ -14,73 +13,71 @@ const Tab = createBottomTabNavigator();
 
 export const Tabs = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarActiveTintColor: COLORS.secondary,
-          tabBarInactiveTintColor: COLORS.gray,
-          tabBarLabelStyle: {
-            fontSize: 12,
-          },
-          tabBarStyle: {
-            height: 70,
-          },
-          tabBarItemStyle: {
-            paddingVertical: 10,
-          },
-          tabBarIcon: ({ color, size, focused, style }) => {
-            let iconName;
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: COLORS.secondary,
+        tabBarInactiveTintColor: COLORS.gray,
+        tabBarLabelStyle: {
+          fontSize: 12,
+        },
+        tabBarStyle: {
+          height: 70,
+        },
+        tabBarItemStyle: {
+          paddingVertical: 10,
+        },
+        tabBarIcon: ({ color, size, focused, style }) => {
+          let iconName;
 
-            if (route.name === 'Home') {
-              iconName = !focused ? 'home-outline' : 'home';
-            } else if (route.name === 'Create') {
-              iconName = !focused ? 'add-circle-outline' : 'add-circle';
-              style = { paddingLeft: 3 };
-              size = 29;
-            } else if (route.name === 'Favorite') {
-              iconName = !focused ? 'heart-outline' : 'heart';
-              size = 26;
-            } else if (route.name === 'Message') {
-              iconName = !focused
-                ? 'chatbubble-ellipses-outline'
-                : 'chatbubble-ellipses';
-            } else if (route.name === 'Profile') {
-              iconName = !focused ? 'person-outline' : 'person';
-              size = 25;
-            }
+          if (route.name === 'Home') {
+            iconName = !focused ? 'home-outline' : 'home';
+          } else if (route.name === 'Create') {
+            iconName = !focused ? 'add-circle-outline' : 'add-circle';
+            style = { paddingLeft: 3 };
+            size = 29;
+          } else if (route.name === 'Favorite') {
+            iconName = !focused ? 'heart-outline' : 'heart';
+            size = 26;
+          } else if (route.name === 'Message') {
+            iconName = !focused
+              ? 'chatbubble-ellipses-outline'
+              : 'chatbubble-ellipses';
+          } else if (route.name === 'Profile') {
+            iconName = !focused ? 'person-outline' : 'person';
+            size = 25;
+          }
 
-            return (
-              <Icon name={iconName} size={size} color={color} style={style} />
-            );
-          },
-        })}
-      >
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'Главная' }}
-        />
-        <Tab.Screen
-          name="Favorite"
-          component={FavoriteScreen}
-          options={{ title: 'Избранное' }}
-        />
-        <Tab.Screen
-          name="Create"
-          component={CreateScreen}
-          options={{ title: 'Создать' }}
-        />
-        <Tab.Screen
-          name="Message"
-          component={MessageScreen}
-          options={{ title: 'Сообщения' }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{ title: 'Профиль' }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+          return (
+            <Icon name={iconName} size={size} color={color} style={style} />
+          );
+        },
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ title: 'Главная' }}
+      />
+      <Tab.Screen
+        name="Favorite"
+        component={FavoriteScreen}
+        options={{ title: 'Избранное' }}
+      />
+      <Tab.Screen
+        name="Create"
+        component={CreateScreen}
+        options={{ title: 'Создать' }}
+      />
+      <Tab.Screen
+        name="Message"
+        component={MessageScreen}
+        options={{ title: 'Сообщения' }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{ title: 'Профиль' }}
+      />
+    </Tab.Navigator>
   );
 };
